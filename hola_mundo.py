edfc254b4ad36a33f7a43b96d9a8d0e4311c0f3f
unknown <kiikeglz.2513@gmail.com>
2021-09-27 23:02:41 -0500

    Mi primer programa

---
+++ hola_mundo.py
@@ -0,0 +1,44 @@
+#message = 'Hola mundo .l.'
+#print(message)
+
+#message = "Adios mundo cruel :´("
+#print(message)
+
+#print("Mensaje super random")
+
+#message1 = 'chupapi muñeño'
+#message2 = 'silenciador moñoñitico'
+
+#print(message1)
+#print(message2)
+
+# variable_con_espacios = "Ejemplirijillo"
+#print(variable_con_espacios)
+
+#Ejemplo cambiar a mayúsculas y minusculas
+#print(message1.upper())
+#print(message2.lower())
+
+#Ejemplo de concatenación
+nombre = "Paco"
+apellido = "Jerte"
+
+#print(nombre + " " + apellido)
+
+nombre_super_completo = nombre + " " + apellido
+
+print(nombre_super_completo)
+
+nombre_f_string = f"{nombre} {apellido}" 
+print(nombre_f_string)
+
+salto_de_ln = "El\npeluca\nsapbeeeeee"
+print(salto_de_ln)  
+
+
+#limpiar cadenas de espacios en blanco
+
+espacio_blanco = "             cual telaraña, te la shupooooo        "
+print(espacio_blanco.lstrip()) 
+print(espacio_blanco.rstrip())
+print(espacio_blanco.strip())
